f6fadb1bea23e9f94cf9425698b27923b5feddf6
Dan Savage <dsavage@crestron.com>
2018-08-14 14:29:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SSharp-Lib-HTTP-GET
---
+++ Sharp/httptest/WWWGo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+using Crestron.SimplSharp.Net.Http;
+
+
+namespace httptest
+{
+	public class WWWGo
+	{
+		public string ipa = "http://10.71.4.128";
+		//public int port = 80;
+		//private string baseUrl;
+        HttpClient myClient = new HttpClient();
+        HttpClientRequest myClientRequest = new HttpClientRequest();
+        HttpClientResponse myClientResponse;
+        
+        public WWWGo()
+	    {
+	    }
+    
+        public string callToWeb()
+        {
+            string result = "";
+            //myClient.Port = port;
+            //result = myClient.Get(IPAddress);
+            //Console.Write("this webpage said: {0}", result);
+            try
+            {
+                myClientRequest.Url.Parse(ipa);
+                myClientResponse = myClient.Dispatch(myClientRequest);
+
+                result = myClientResponse.ContentString;
+            }
+            catch(Exception e)
+            {
+                CrestronConsole.PrintLine("Something went wrong = {0}", e);
+            }
+            return (result);
+        }
+    }
+}
